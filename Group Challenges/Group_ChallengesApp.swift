016ee8047ebd2d3d1418f63199883b54790f2757
jakilgo <justin.kilgo314@gmail.com>
2023-04-30 16:45:11 -0500

    Initial Commit

---
+++ Group Challenges/Group_ChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_ChallengesApp.swift
+//  Group Challenges
+//
+//  Created by Justin Kilgo on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Group_ChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
